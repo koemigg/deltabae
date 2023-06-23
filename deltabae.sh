ea0264940a49ef1dc6aa18debb7c4fd94511aa4e
Harxxki <haruki4dev@gmail.com>
2023-06-23 18:59:50 +0900

    ✨ Update to receive parameters.

--- deltabae.sh
+++ deltabae.sh
@@ -1,2 +1,39 @@
 #!/usr/bin/zsh
-mkdir -p ./tmp && delta ./a.txt ./b.txt --side-by-side --syntax-theme="Nord" --zero-style="#333" | ansifilter --html --encoding "utf-8" > ./tmp/diff.html
+
+# Set default values
+output_dir="./tmp"
+html_name="diff"
+file1=""
+file2=""
+
+# Parse command line arguments
+while getopts 'f:s:o:n:' flag; do
+  case "${flag}" in
+    f) file1="${OPTARG}" ;;
+    s) file2="${OPTARG}" ;;
+    o) output_dir="${OPTARG}" ;;
+    n) html_name="${OPTARG}" ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
+
+# Check if file1 and file2 are specified
+if [ -z "$file1" ] || [ -z "$file2" ]; then
+    echo "Both -f (file1) and -s (file2) options must be specified."
+    exit 1
+fi
+
+# Check if file1 and file2 exist
+if [ ! -e "$file1" ] || [ ! -e "$file2" ]; then
+    echo "One or both of the files specified do not exist."
+    exit 1
+fi
+
+# Check if directory should be created
+if [ ! -z "$output_dir" ] ; then
+    mkdir -p $output_dir
+fi
+
+# Run delta and ansifilter
+delta $file1 $file2 --side-by-side --syntax-theme="Nord" --zero-style="#333" | ansifilter --html --encoding "utf-8" > "$output_dir/$html_name.html"
+
